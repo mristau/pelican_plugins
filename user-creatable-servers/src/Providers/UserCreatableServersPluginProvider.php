65198ee80190583f1cfe67f83a50381ef36b1ee8
Boy132 <mail@boy132.de>
2025-12-20 21:35:34 +0100

    add policy and role permissions for user resource limits
    closes #26

--- user-creatable-servers/src/Providers/UserCreatableServersPluginProvider.php
+++ user-creatable-servers/src/Providers/UserCreatableServersPluginProvider.php
@@ -6,6 +6,7 @@ use App\Enums\HeaderActionPosition;
 use App\Enums\HeaderWidgetPosition;
 use App\Filament\Admin\Resources\Users\UserResource;
 use App\Filament\App\Resources\Servers\Pages\ListServers;
+use App\Models\Role;
 use Boy132\UserCreatableServers\Filament\Admin\Resources\Users\RelationManagers\UserResourceLimitRelationManager;
 use Boy132\UserCreatableServers\Filament\App\Widgets\UserResourceLimitsOverview;
 use Boy132\UserCreatableServers\Filament\Components\Actions\CreateServerAction;
@@ -20,6 +21,9 @@ class UserCreatableServersPluginProvider extends ServiceProvider
         ListServers::registerCustomHeaderWidgets(HeaderWidgetPosition::Before, UserResourceLimitsOverview::class);
 
         ListServers::registerCustomHeaderActions(HeaderActionPosition::Before, CreateServerAction::make());
+
+        Role::registerCustomDefaultPermissions('userResourceLimits');
+        Role::registerCustomModelIcon('userResourceLimits', 'tabler-cube-plus');
     }
 
     public function boot(): void {}
